794a4d0a52542dd1da3ac3083a4e1dd0131fe639
ShreyasVH <shreyas.deve.1@gmail.com>
2025-10-26 23:36:33 +0530

    table support

---
+++ src/containers/table.svelte
@@ -0,0 +1,51 @@
+<script>
+    import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+
+    const columns = [
+        'Country',
+        'Capital',
+        'Currency',
+        'Continent'
+    ];
+
+    const data = [
+        {
+            country: 'India',
+            capital: 'New Delhi',
+            currency: 'Rupee',
+            continent: 'Asia'
+        },
+        {
+            country: 'United Kingdom',
+            capital: 'London',
+            currency: 'Pound',
+            continent: 'Europe'
+        }
+    ];
+</script>
+
+<div>
+    <DataTable style="width: 100%">
+        <Head>
+            <Row>
+                {#each columns as column}
+                    <Cell head>
+                        {column}
+                    </Cell>
+                {/each}
+            </Row>
+        </Head>
+
+        <Body>
+            {#each data as row}
+                <Row>
+                    {#each columns as column}
+                    <Cell>
+                        {row[column.toLowerCase()]}
+                    </Cell>
+                    {/each}
+                </Row>
+            {/each}
+        </Body>
+    </DataTable>
+</div>
